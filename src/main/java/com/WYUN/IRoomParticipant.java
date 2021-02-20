5a71692197b667db026e4ae15806eaf2b139a37b
ystt-lita <teppei.yoshikawa@litalico.co.jp>
2021-02-20 17:16:00 +0900

    add license notice on each source file

--- src/main/java/com/WYUN/IRoomParticipant.java
+++ src/main/java/com/WYUN/IRoomParticipant.java
@@ -1,6 +1,20 @@
 /***************************************************
  * Copyright 2020, 2021 EwdErna
  * LICENSE: ../../../../../LICENSE
+ * This file is part of WYUN Server.
+ * 
+ *     WYUN Server is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WYUN Server is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with WYUN Server.  If not, see <https://www.gnu.org/licenses/>.
  ***************************************************/
 package com.WYUN;
 
